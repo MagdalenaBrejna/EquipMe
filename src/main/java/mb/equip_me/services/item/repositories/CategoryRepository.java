892b06e33211897552763359f8d87c78de81808a
MagdalenaBrejna <magdalena.brejna@wp.pl>
2022-12-14 21:09:20 +0100

    feat: Create CategoryRepository
    Create CategoryRepository that extends JpaRepository.

---
+++ src/main/java/mb/equip_me/services/item/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package mb.equip_me.services.item.repositories;
+
+import mb.equip_me.services.item.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
